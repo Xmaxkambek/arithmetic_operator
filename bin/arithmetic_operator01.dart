d7edd2752b86985f0159fec82ca30b18d9803784
nnn <maxkambekxolbekov515@gmail.com>
2022-07-21 22:35:47 -0700

    01

--- bin/arithmetic_operator01.dart
+++ bin/arithmetic_operator01.dart
@@ -7,4 +7,8 @@ Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
 */
 void main() {
   //write your code here
+  int num_one = 5;
+  int num_two = 4;
+  int ans=num_one+num_two;
+  print(ans);
 }
